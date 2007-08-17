b991db6830500ecb6ef025b3ebc2b609e24bfe92
Ricardo SIGNES <rjbs@cpan.org>
2007-08-17 19:06:52 -0400

    more documentation, adhere to some usual standards

--- lib/App/Config.pm
+++ lib/App/Config.pm
@@ -9,7 +9,7 @@ use File::HomeDir qw(home);
 use YAML qw(LoadFile);
 
 use Sub::Exporter -setup => {
-  groups     => [ setup => \'_build_config_methods' ],
+  groups => [ setup => \'_build_config_methods' ],
 };
 
 =head1 NAME
@@ -18,15 +18,16 @@ App::Config - Easy configuration base class for your App.
 
 =head1 VERSION
 
-Version 0.01
+version 0.001
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.001';
 
 =head1 SYNOPSIS
 
   package YourApplication::Config;
+
   use App::Config -setup => {
     filename => 'something.yaml',
     template => {
@@ -39,8 +40,8 @@ our $VERSION = '0.01';
 Elsewhere...
 
   use YourApplication::Config 'my_instance_config.yml';
-  my $foo = YourApplication::Config->foo;
 
+  my $foo = YourApplication::Config->foo;
 
 =head1 DESCRIPTION
 
@@ -49,17 +50,84 @@ app. The basic implementation stores its configuration in YAML in a text
 file found in all the usual places. By default, App::Config looks for
 myapp.yml, but an alternate filename may be passed when using the module.
 
-This was initially Rubric::Config by RJBS C<< <rjbs at cpan.org> >>.
+This module was derived from L<Rubric::Config|Rubric::Config>.
+
+=head1 USING APP::CONFIG
+
+The L</SYNOPSIS> section, above, demonstrates an example of almost every
+feature of App::Config.  It's a very simple module with a very small interface.
+
+It is not a base class.  It is a utility for setting up a class that stores
+loaded configuration data.  You just need to C<use> the module like this:
+
+  package Your::Config;
+
+  use App::Config -setup => {
+    filename => 'your_program.yaml',
+    template => {
+      username => undef,
+      hostname => undef,
+      logfile  => undef,
+      facility => 'local1',
+      path     => [ qw(/var/spool /tmp/jobs) ],
+    },
+  };
+
+When another module uses Your::Config, F<your_program.yaml> will be loaded and
+its contents will be merged over the defaults given by the C<template>
+argument.  Each entry in the template hashref becomes a method on
+YourProgram::Config, which returns either the value from the config file or the
+value from the template, if no entry exists in the config file.
+
+So, assuming that F<your_program.yaml> looks like this:
+
+  ---
+  username: rjbs
+  hostname: fulfill.example.com
+
+  path:
+    - /var/spool/jobs
+    - /home/rjbs/spool/jobs
+
+Then these are the results of method calls on Your::Config:
+
+  Your::Config->username; # 'rjbs'
+
+  Your::Config->logfile;  # undef
+
+  Your::Config->facility; # 'local0'
+
+  Your::Config->path;     # qw(/var/spool/jobs  /home/rjbs/spool/jobs)
+
+The configuration file is found by looking for the given name in the following
+paths:
+
+  ./
+  ../
+  ~/
+  /usr/local/etc/
+  /etc/
+
+If you'd rather look for a different filename, you can specify it when using
+Your::Config:
+
+  use Your::Config 'alt.yaml';
+
+When Your::Config is loaded in this way, it will look for the given filename
+instead of the default one.  If it is loaded again with a different filename
+given, an exception will be raised.
 
 =head1 METHODS
 
-=head2 _build_config_methods
-
-Initialize all the methods for our new class.
+App::Config doesn't actually have any real public methods of its own.  Its
+methods are all private, and serve to power its C<import> routine.  These will
+probably be exposed in the future to allow for subclassing of App::Config, but
+in the meantime, don't rely on them.
 
 =cut
 
-# this is a group generator
+# Initialize all the methods for our new class.
+# this is a Sub::Exporter group generator
 sub _build_config_methods {
   my ($self, $name, $arg) = @_;
 
@@ -87,34 +155,22 @@ sub _build_config_methods {
   return \%sub;
 }
 
-
-=head2 _build_accessor
-
-Build accessors for all of $template
-
-=cut
-
+# Build accessors for all of $template
 sub _build_accessor {
   my ($class, $attr, $arg) = @_;
   return sub {
     my $self  = shift;
     
-  	exists $self->config_from_file->{$attr}
-			? $self->config_from_file->{$attr}
-			: $arg->{$attr}
+    exists $self->config_from_file->{$attr}
+         ? $self->config_from_file->{$attr}
+         : $arg->{$attr}
   };
 }
 
-=head2 _build_config_filename
-
-Returns the method later referred to as config_filename.
-
-config_filename, deduces what the name of the config file it should look for
-will be.  Was it passed in, or should we guess based on the module name?
-
-=cut
-
-
+# Returns the method later referred to as config_filename.
+#
+# config_filename, deduces what the name of the config file it should look for
+# will be.  Was it passed in, or should we guess based on the module name?
 sub _build_config_filename {
   my ($class, $arg) = @_;
 
@@ -132,15 +188,11 @@ sub _build_config_filename {
   }
 }
 
-=head2 _build_config_from_file
-
-Returns the method later referred to as config_from_file.
-
-config_from_file returns the config data, if loaded.  If it hasn't already been
-loaded, it finds and parses the configuration file, then returns the data.
-
-=cut
-
+# Returns the method later referred to as config_from_file.
+#
+# config_from_file returns the config data, if loaded.  If it hasn't already
+# been loaded, it finds and parses the configuration file, then returns the
+# data.
 sub _build_config_from_file {
   my ($app_config, $arg) = @_;
  
@@ -157,23 +209,23 @@ sub _build_config_from_file {
     my $path = dirname(realpath($0));
 
     # TODO will make this configurable later
-    my @locations = ( 
-                      '',
-                      "./",
-                      "../",
-                      "$path/",
-                      "$path/../etc/",
-                      "$home/.",
-                      "/usr/local/etc/",
-                      "/etc/",
-                    );
+    my @locations = (
+      q{},
+      q{./},
+      q{../},
+      qq{$path/},
+      qq{$path/../etc/},
+      qq{$home/.},
+      q{/usr/local/etc/},
+      q{/etc/},
+    );
 
     my $config_filename = $class->config_filename; 
+
     do {
       die "Config file not found: $config_filename" unless scalar(@locations);
       my $location = shift @locations;
       $config_file = $location . $config_filename;
-      #warn $config_file . "\n";
     } until -e $config_file;
 
     my $file_data = LoadFile($config_file);
@@ -187,7 +239,6 @@ sub _build_config_from_file {
 sub _merge_data {
   my ($self, $template, $override) = @_;
   
-
   my $merged = {};
   for (keys %$template) {
     $merged->{$_} = defined $override->{$_} ? $override->{$_} : $template->{$_};
@@ -198,6 +249,7 @@ sub _merge_data {
 
 sub _build_import {
   my ($app_config, $arg) = @_;
+
   return sub {
     my ($self, $filename) = @_;
     $arg->{filename} = $filename if $filename;
@@ -207,15 +259,15 @@ sub _build_import {
 =head1 AUTHOR
 
 John Cappiello, C<< <jcap at cpan.org> >>
+
 Ricardo SIGNES, C<< <rjbs at cpan.org> >>
 
 =head1 BUGS
 
-Please report any bugs or feature requests to
-C<bug-app-config at rt.cpan.org>, or through the web interface at
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=App-Config>.
-I will be notified, and then you'll automatically be notified of progress on
-your bug as I make changes.
+Please report any bugs or feature requests through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=App-Config>.  I will be
+notified, and then you'll automatically be notified of progress on your bug as
+I make changes.
 
 =head1 SUPPORT
 
@@ -226,10 +278,6 @@ You can find documentation for this module with the perldoc command.
 You can also look for information at:
 
 =over 4
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/App-Config>
 
 =item * CPAN Ratings
 
@@ -245,21 +293,31 @@ L<http://search.cpan.org/dist/App-Config>
 
 =back
 
+=head1 TODO
+
+=over 4
+
+=item * a ->new method to allow loading different configs
+
+=item * a way to tell Your::Config, with no explicit filename, to die unless a filename was specified by an earlier use
+
+=back
+
 =head1 ACKNOWLEDGEMENTS
 
-RJBS C<< <rjbs at cpan.org> >> not only wrote the inspiration for this in
-Rubric::Config, but he also basically wrote the majority of the implementation
-here, and even provided extensions of what he knew I wanted it to do, even when
-I said I didn't need that yet. In the end it ended up being extremely elegant,
-which I can say without being boastful, because he wrote the elegant bits.
+Ricardo SIGNES not only wrote the inspiration for this in Rubric::Config, but
+he also basically wrote the majority of the implementation here, and even
+provided extensions of what he knew I wanted it to do, even when I said I
+didn't need that yet. In the end it ended up being extremely elegant, which I
+can say without being boastful, because he wrote the elegant bits.
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2006 John Cappiello, all rights reserved.
+Copyright 2006, John Cappiello.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
 
 =cut
 
-1; # End of App::Config
+1;
--- t/02-custom-filename.t
+++ t/02-custom-filename.t
@@ -5,7 +5,7 @@ use Test::More tests => 3;
 use lib 't/lib';
 
 BEGIN {
-	use_ok( 'MyApp::Config', 't/etc/my_custom.yml' );
+	use_ok( 'MyApp::Config', 't/etc/mycustom.yml' );
 }
 
 is( MyApp::Config->hostname, 'localhost', 'Default config value expected');
